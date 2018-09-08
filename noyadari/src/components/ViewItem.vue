c285777287b30dcbaeaf4f4ff4c23691c8d2b337
avishayp <avishay@nanoscentlabs.com>
2018-09-09 01:01:38 +0300

    Add close button to view page

--- noyadari/src/components/ViewItem.vue
+++ noyadari/src/components/ViewItem.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
+
+    <b-button id="spaced" href="/home">&times; close</b-button>
+
     <b-card :title="'SN: ' + item.sn"
             :sub-title="'name:' + item.name">
+
+      <p class="card-text">{{item.details}}</p>
 
       <b-table responsive flex striped hover
                :items="ocEvents"
                :fields="fields">              
       </b-table>
     </b-card>
+
   </div>
 </template>
 
@@ -48,5 +54,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  #spaced {
+    margin-top: 10px;
+    margin-bottom: 10px
+  }
 </style>
